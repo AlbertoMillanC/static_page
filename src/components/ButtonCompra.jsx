aa068aa167c16ac4996ce50e49222956e3869cb9
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2022-12-31 20:33:00 -0500

    styles footer add buttons

--- src/components/ButtonCompra.jsx
+++ src/components/ButtonCompra.jsx
@@ -2,25 +2,33 @@ import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 function Button() {
   const [comprar, setComprar] = useState(false);
   const notify = () => toast("Gracias por suscribirse");
 
   return (
-    <div class="flex">
-      <button
-        
-        onClick={() => {
-          toast.success("Gracias por su compra");
-          setComprar(!comprar);
-        }}
-      >
-        {comprar ? " Compra Aquí" : "Compra Aquí"}
-      </button>
-      <p></p>
-      <div class= "px-40">
-      <button   onClick={notify}>subscribirse</button>
+    <div class="    md:flex  md:flex-auto ">
+      <div class=" container grid grid-flow-col p-2  md:flex flex-auto px-20 > ">
+        <button
+          onClick={() => {
+            toast.success("Gracias por su compra");
+            setComprar(!comprar);
+          }}
+        >
+          {comprar ? "comprar" : "Comprar"}
+        </button>
+      </div>
+      <div class=" container grid grid-flow-col p-2  md:flex flex-auto px-20 ">
+        <button onClick={notify}>subscribirse</button>
+      </div>
+      <div class="  container grid grid-flow-col p-2  md:flex flex-auto px-20 ">
+        <button onClick={notify}>subscribirse</button>
+      </div>
+      <div class="  container grid grid-flow-col p-2  md:flex flex-auto px-20  ">
+        <button onClick={notify}>subscribirse</button>
+      </div>
+      <div class="  container grid grid-flow-col p-2  md:flex flex-auto px-20 ">
+        <button onClick={notify}>subscribirse</button>
       </div>
       <ToastContainer />
     </div>
@@ -28,5 +36,3 @@ function Button() {
 }
 
 export default Button;
-
-  
