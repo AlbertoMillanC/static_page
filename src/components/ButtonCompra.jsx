8f5743a222e590e267acc07d05c1c7dd107549d8
Alberto Millán <90913818+AlbertoMillanC@users.noreply.github.com>
2023-01-03 02:15:39 -0500

    add styles  and button footer and politicas de cookies y condiciones de la pagina- button-

--- src/components/ButtonCompra.jsx
+++ src/components/ButtonCompra.jsx
@@ -9,7 +9,6 @@ function Button() {
   return (
     <div class=" md:flex  ">
       <div class="    text-custom-color mt-1 text-center   md:flex-auto px-20    ">
-        
         <button
           onClick={() => {
             toast.success("Gracias por su compra");
@@ -20,23 +19,24 @@ function Button() {
         </button>
       </div>
       <div class="  text-custom-color mt-1 text-center   md:flex-auto px-20    ">
-        <button onClick={notify}>subscribirse</button>
-        <ul class= "text-gray-600">
-        <li>es </li>
-        <li>es </li>
-        <li>es </li>
+        <button onClick={notify}>Legales</button>
+      <div>
+      <ul class="text-gray-600  grid ">
+          <button class="border-r-1 mt-1">Política de privacidad </button>
+          <button class="border-r-1 mt-1">Términos y condiciones </button>
+          <button class="border-r-1 mt-1">Política de Cookies </button>
+          <button class="border-r-1 mt-1">📖Libro de reclamaciones </button>
         </ul>
-        
-
+      </div>
       </div>
       <div class="  text-custom-color mt-1 text-center   md:flex-auto px-20     ">
-        <button onClick={notify}>subscribirse</button>
+        <button onClick={notify}>Subscribirse</button>
       </div>
       <div class="  text-custom-color mt-1 text-center   md:flex-auto px-20     ">
-        <button onClick={notify}>subscribirse</button>
+        <button onClick={notify}>Contacto</button>
       </div>
       <div class=" text-custom-color mt-1 text-center   md:flex-auto px-20       ">
-        <button onClick={notify}>subscribirse</button>
+        <button onClick={notify}>Redes Sociales</button>
       </div>
       <ToastContainer />
     </div>
@@ -44,5 +44,3 @@ function Button() {
 }
 
 export default Button;
-
-
